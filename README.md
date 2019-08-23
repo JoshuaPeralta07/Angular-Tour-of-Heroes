1df7d189499d90490b2ee497979a51de53aaad02
JoshuaPeralta07 <peralta.joshuapaul07@gmail.com>
2019-08-23 14:28:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8dutzn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8dutzn)
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Dr. Strange'},
+  {id: 12, name: 'Spider-man'},
+  {id: 13, name: 'Iron Man'},
+  {id: 14, name: 'Thor'},
+  {id: 15, name: 'Captain America'},
+  {id: 16, name: 'Black Widow'},
+  {id: 17, name: 'Rocket Racoon'},
+  {id: 18, name: 'Hulk'},
+  {id: 19, name: 'Hawkeye'},
+  {id: 20, name: 'Ant Man'}
+];
